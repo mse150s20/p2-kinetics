76e0cc6bc5fb4efaaed91325d84f1f17ec336354
Sam Piraino <sampiraino@u.boisestate.edu>
2020-04-06 20:51:20 -0400

    returned to original

--- kinetics.py
+++ kinetics.py
@@ -16,9 +16,9 @@ def kinetics(k1, k2, a_0, b_0, c_0):
         return [-k1*A*B/(A+B+C), k1*A*B/(A+B+C)-k2*B, k2*B]
     return solve_ivp(abc, [0, maxtime], [a_0,b_0,c_0], t_eval=np.arange(0, maxtime, 1),method='Radau')
 
-solution = kinetics(0.1,0.02,N-1,1,0)
+# Original k1 is 0.1
+solution = kinetics(0.5,0.02, N-1,1,0)
 
-This is not a TEST
 
 plt.plot(solution.t,solution.y[0],label='A')
 plt.plot(solution.t,solution.y[1],label='B',linestyle='dashed')
@@ -28,5 +28,3 @@ plt.ylabel('Population')
 plt.grid() 
 plt.legend()
 plt.show()
-
-
