f377020a4243f32c66e580425cc64702922cfd2b
Ashton <ashtonenrriques@u.boisestate.edu>
2020-04-09 16:39:13 -0400

    Deleted contibutors file and consolidated in contributors.txt, moved idaho_infections.csv into own sub-directory to help in graphing program. kinetics.py is not intended to be 'ready' to replace main one as I'm working on adding the actual curve for Idaho cases to the graph output

--- kinetics.py
+++ kinetics.py
@@ -6,7 +6,7 @@ import numpy as np
 maxtime = 365
 # people in Idaho
 N = 1754000
-def kinetics(k1, k2, a_0, b_0, c_0):
+def kinetics(k1, k2, a_0, b_0, c_0):   #Units of k1 
     def abc(t, y):
         ''' System of differential equations: y(t) = [A(t),B(t),C(t)]
             returns:
@@ -15,9 +15,13 @@ def kinetics(k1, k2, a_0, b_0, c_0):
         A,B,C = y
         return [-k1*A*B/(A+B+C), k1*A*B/(A+B+C)-k2*B, k2*B]
     return solve_ivp(abc, [0, maxtime], [a_0,b_0,c_0], t_eval=np.arange(0, maxtime, 1),method='Radau')
-solution = kinetics(0.07,0.02, N-1000,1000,0) #ONE infected person on day0
+solution = kinetics(0.07,0.01, N-1000,1000,0) #ONE infected person on day0
 
-
+data='idaho_infections/idaho_infections.csv'
+data2=np.delete(data, [0,1], axis=0)
+x1=data2[:,0]
+y1=data2[:,2]
+plt.plot(x1,y1, label='R')
 plt.plot(solution.t,solution.y[0],label='A')
 plt.plot(solution.t,solution.y[1],label='B',linestyle='dashed')
 plt.plot(solution.t,solution.y[2],label='C',linestyle='dotted')
