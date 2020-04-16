2e5e639ea084ee853616efc6206cc5b7543bb7b9
Hayes Neukom <hayesneukom@u.boisestate.edu>
2020-04-16 13:21:39 -0400

    I added some questions to the kinetics.py trying to fix it and figure out where numbers came from.

--- kinetics.py
+++ kinetics.py
@@ -6,7 +6,7 @@ import sys
 # the maximum time of virus in days
 maxtime = 365
 # people in Idaho
-N = 1754000
+N = 1754000 #where did this number come from?
 def kinetics(k1, k2, a_0, b_0, c_0):
     def abc(t, y):
         ''' System of differential equations: y(t) = [A(t),B(t),C(t)]
@@ -20,7 +20,7 @@ def kinetics(k1, k2, a_0, b_0, c_0):
 
 # Original k1 is 0.1
 
-filename = idaho_infected
+filename = idaho_infections
 infected = np.loadtxt(filename, skiprows = 3, delimiter = ',', usecols = (2))
 
 solution = kinetics(0.5,0.02, N-1,1,0)
