b2c2d7631053b591ee5c5cfe3c2cc9fcf8d57ba2
Taylor Nowling <taylornowling@u.boisestate.edu>
2020-04-20 12:22:00 -0400

    Attempting to create a log line in the infections plot

--- plot-infections.py
+++ plot-infections.py
@@ -7,8 +7,10 @@ filename= sys.argv[1]
 infected= np.loadtxt(filename, skiprows =2, delimiter = ',', usecols=(0,2))
 infected_people= infected[:,1]
 day= infected[:,0]
+log_infected = np.log(infected_people)
 
-plt.plot(day, infected_people)
+plt.plot(day, infected_people, color='black', linestyle='-')
+plt.plot(day, log_infected, color='green', linestyle='--')
 plt.title("Infection rate of COVID-19 in Idaho") 
 plt.xlabel('Time [days]')
 plt.ylabel('Population infected')
