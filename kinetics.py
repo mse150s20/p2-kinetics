45c71271f3c19827df0fa382d790d968051e6ade
Eric Jankowski <ericjankowski@boisestate.edu>
2020-04-20 12:24:56 -0400

    Got the existing data plotted against our model on the same plot.

--- kinetics.py
+++ kinetics.py
@@ -19,17 +19,13 @@ def kinetics(k1, k2, a_0, b_0, c_0):   #Units of k1
     return solve_ivp(abc, [0, maxtime], [a_0,b_0,c_0], t_eval=np.arange(0, maxtime, 1),method='Radau')
 # Original k1 is 0.1
 filename = sys.argv[1]
-population_infected = np.loadtxt(filename, skiprows = 3, delimiter = ',', usecols = (2))
+population_infected = np.loadtxt(filename, skiprows = 3, delimiter = ',', usecols = (0,2))
 time = np.loadtxt(filename, skiprows = 3, delimiter = ',', usecols =(0))
 #filename = idaho_infections.csv
 #infected = np.loadtxt(filename, skiprows = 3, delimiter = ',', usecols = (2))
 solution = kinetics(0.5,0.02, N-1,1,0) #ONE infected person on day0
 
-data='idaho_infections/idaho_infections.csv'
-data2=np.delete(data, [0,1], axis=0)
-x1=data2[:,0]
-y1=data2[:,2]
-plt.plot(x1,y1, label='R')
+plt.plot(population_infected[:,0],population_infected[:,1], label='R')
 plt.plot(solution.t,solution.y[0],label='A')
 plt.plot(solution.t,solution.y[1],label='B',linestyle='dashed')
 plt.plot(solution.t,solution.y[2],label='C',linestyle='dotted')
