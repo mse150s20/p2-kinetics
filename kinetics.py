ab97826fc6e85469d501df56bf7f34fad9e89b65
Hayes Neukom <hayesneukom@u.boisestate.edu>
2020-04-20 11:15:04 -0400

    I forgot what i did

--- kinetics.py
+++ kinetics.py
@@ -21,8 +21,7 @@ def kinetics(k1, k2, a_0, b_0, c_0):   #Units of k1
 filename = sys.argv[1]
 population_infected = np.loadtxt(filename, skiprows = 3, delimiter = ',', usecols = (2))
 time = np.loadtxt(filename, skiprows = 3, delimiter = ',', usecols =(0))
-#filename = idaho_infections.csv
-#infected = np.loadtxt(filename, skiprows = 3, delimiter = ',', usecols = (2))
+
 solution = kinetics(0.5,0.02, N-1,1,0) #ONE infected person on day0
 
 data='idaho_infections/idaho_infections.csv'
