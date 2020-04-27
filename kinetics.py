4b2d7b13b7cab1e6cf56e93346298a615ef49cc9
Cody LaCoursiere <codylacoursiere@u.boisestate.edu>
2020-04-27 13:16:18 -0400

    Changed the A variable to H and defined it.

--- kinetics.py
+++ kinetics.py
@@ -14,8 +14,8 @@ def kinetics(k1, k2, a_0, b_0, c_0):   #Units of k1
             returns:
                 A list containing [dA/dt, dB/dt, dC/dt]
         '''
-# where A is uninfected, B is infected, C is recovered
-        A,B,C = y
+        H,B,C = y
+	#H = the number of healthy people who have never contracted the virus.
         return [-k1*A*B/(A+B+C), k1*A*B/(A+B+C)-k2*B, k2*B] #The equation for Dead will just be (k3*B) where k3 is the death rate, we will have to add (-k3*B) to the infected part of the graph.
     return solve_ivp(abc, [0, maxtime], [a_0,b_0,c_0], t_eval=np.arange(0, maxtime, 1),method='Radau')
 # Original k1 is 0.1
