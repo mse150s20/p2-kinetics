1636c75d3d14ea060d16b062c45bc2ab14c7f9b7
Shannon Crain <shannoncrain@u.boisestate.edu>
2020-04-28 01:09:47 -0400

    Did more playing with k1 and saw that it affects when the disease begins to affect most of the population

--- README.md
+++ README.md
@@ -44,7 +44,7 @@ A graph of the number of infections vs time on a logarithmic scale.
 
 
 # Results from playing with parameters
-Increasing k1 increases the rate at which uninfected people are infected, increasing k2 increases the rate at which people recover from the infected portion of the graph, and increasing k3 increases the rate at which people die from the infected portion of the graph.
+The most apparent effect of decreasing k1 was delaying the "ramping up" of the population
 
 # Info copy pasted from info.txt - should be cleaned up
 Project 2 descriptions:
@@ -68,4 +68,4 @@ Species A:
 This reaction depends on the concentration of A and the concentration of B
 concentration of A is A/(A+B+C), and the concentration of B is B/(A+B+C)
 
-k1 and k2 are our reaction constants
+k1 and k2 are our reaction constants - what do they mean?
--- kinetics.py
+++ kinetics.py
@@ -7,7 +7,7 @@ import sys
 # the maximum time of virus in days
 maxtime = 365
 # people in Idaho
-N = 1754000 # Population of Idaho according to U.S. Census
+pop = 1754000 # Population of Idaho according to U.S. Census
 def kinetics(k1, k2, h_0, i_0, r_0):   #Units of k1 
     def abc(t, y):
         ''' System of differential equations: y(t) = [H(t),I(t),R(t)]
@@ -29,7 +29,7 @@ time = np.loadtxt(filename, skiprows = 8, delimiter = ',', usecols = (0))
 infected = np.loadtxt(filename, skiprows = 8, delimiter = ',', usecols = (2))
 dead = np.loadtxt(filename, skiprows = 8, delimiter = ',', usecols = (4))
 recovered = np.loadtxt(filename, skiprows = 8,  delimiter = ',', usecols = (6))
-solution = kinetics(0.5,0.02, N-1,1,0) #ONE infected person on day0
+solution = kinetics(0.05,0.02, pop-1,1,0) #ONE infected person on day0
 
 plt.plot(time, dead, label='Real Dead', color = 'k')
 plt.plot(time,infected, label='Real Infections')
