7dce9fc3e7908acfb18dd41eea3bfddf416ab642
Cody LaCoursiere <codylacoursiere@u.boisestate.edu>
2020-04-29 17:08:21 -0400

    Centered the title of the first graph.

--- plot-infections.py
+++ plot-infections.py
@@ -7,7 +7,7 @@ filename= sys.argv[1]
 infected = np.loadtxt(filename, skiprows =8, delimiter = ',', usecols=(0,2))
 infected_people= infected[:,1]
 day= infected[:,0]
-plt.title("Infection rate of COVID-19 in Idaho") 
+plt.title("Infection rate of COVID-19 in Idaho", loc= 'center') 
 dead= np.loadtxt(filename, skiprows =8, delimiter = ',', usecols=(0,4))
 dead_people= dead[:,1]
 day = dead[:,0]
