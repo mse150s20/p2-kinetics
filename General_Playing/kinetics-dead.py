be5bbd8e42d820bcdab7d99a8695af65b64bede5
Shannon Crain <shannoncrain@u.boisestate.edu>
2020-04-29 21:40:31 -0400

    Gave kinetics-dead.py a more realistic k3 for death rate

---
+++ General_Playing/kinetics-dead.py
@@ -0,0 +1,33 @@
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+import numpy as np
+
+# the maximum time of virus in days
+maxtime = 65
+# people in Idaho
+N = 1754000
+def kinetics(k1, k2, k3, a_0, b_0, c_0, d_0):
+    def abc(t, y):
+        ''' System of differential equations: y(t) = [A(t),B(t),C(t)]
+            returns:
+                A list containing [dA/dt, dB/dt, dC/dt]
+        '''
+# where A is uninfected, B is infected, C is recovered, and D is dead
+        A,B,C,D = y
+        return [-k1*A*B/(A+B+C+D), k1*A*B/(A+B+C+D)-k2*B-k3*B, k2*B, k3*B]
+    return solve_ivp(abc, [0, maxtime], [a_0,b_0,c_0,d_0], t_eval=np.arange(0, maxtime, 1),method='Radau')
+
+solution = kinetics(0.139,0.02,0.003167, N-1,1,0,0) # The numbers are 56 dead over 1768 infected currently as of 04-26-20 if there is a findings team member who has the actual death rate, go ahead and change it. Infection rate from www.worldometers.info/coronavirus.
+
+
+#plt.plot(solution.t,solution.y[0],label='Uninfected')
+plt.plot(solution.t,solution.y[1],label='Infected',linestyle='dashed')
+plt.plot(solution.t,solution.y[2],label='Recovered',linestyle='dotted')
+plt.plot(solution.t,solution.y[3],label='Dead',linestyle= '-.',color='Black')
+plt.xlabel('Time [days]')
+plt.ylabel('Population infected')
+plt.grid() 
+plt.legend()
+plt.show()
+plt.title('States of Covid-19 in the Public')
+
