48d98f6c96f0fc4bbb7e742bd68f12b00777d5cd
Shannon Crain <shannoncrain@u.boisestate.edu>
2020-04-29 22:01:03 -0400

    Replaced D in kinetics-dead with a more descriptive variable name

--- General_Playing/kinetics-dead.py
+++ General_Playing/kinetics-dead.py
@@ -14,8 +14,8 @@ def kinetics(rateInfect, rateRecover, rateDie, a_0, b_0, c_0, d_0):
                 A list containing [dA/dt, dB/dt, dC/dt]
         '''
 # where A is uninfected, B is infected, C is recovered, and D is dead
-        healthy,infected,recovered,D = y
-        return [-rateInfect*healthy*infected/(healthy+infected+recovered+D), rateInfect*healthy*infected/(healthy+infected+recovered+D)-rateRecover*infected-rateDie*infected, rateRecover*infected, rateDie*infected]
+        healthy,infected,recovered,dead = y
+        return [-rateInfect*healthy*infected/(healthy+infected+recovered+dead), rateInfect*healthy*infected/(healthy+infected+recovered+dead)-rateRecover*infected-rateDie*infected, rateRecover*infected, rateDie*infected]
     return solve_ivp(abc, [0, maxtime], [a_0,b_0,c_0,d_0], t_eval=np.arange(0, maxtime, 1),method='Radau')
 
 solution = kinetics(0.139,0.02,0.003167, N-1,1,0,0) # The numbers are 56 dead over 1768 infected currently as of 04-26-20 if there is a findings team member who has the actual death rate, go ahead and change it. Infection rate from www.worldometers.info/coronavirus.
