0a0802bc7d0e6c4c253294aa786ac0d1fd565c7d
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2020-05-01 14:44:31 -0600

    added back in a plot of the model and save that fig

--- Real_Data.py
+++ Real_Data.py
@@ -5,39 +5,64 @@ import sys
 
 # the maximum time of virus in days
 maxtime = 365
-pop = 1754000 # Population of Idaho according to U.S. Census
+pop = 1754000  # Population of Idaho according to U.S. Census
+
+
 def kinetics(rateInfect, rateRecover, h_0, i_0, r_0):
-        #Rates in individuals per day
-        #rateInfect = the rate at which healthy people contract the virus.
-        #rateInfect units = people/day
-        #rateRecover = the rate at which infected people recover from the virus.
-        #rateRecover units = people/day
+    # Rates in individuals per day
+    # rateInfect = the rate at which healthy people contract the virus.
+    # rateInfect units = people/day
+    # rateRecover = the rate at which infected people recover from the virus.
+    # rateRecover units = people/day
     def abc(t, y):
-        ''' System of differential equations: y(t) = [Healthy(t),Infected(t),Recovered(t)]
+        """ System of differential equations: y(t) = [Healthy(t),Infected(t),Recovered(t)]
             returns:
                 A list containing [dHealthy/dt, dInfected/dt, dRecovered/dt]
-        '''
-        healthy,infected,recovered = y
-        return [-rateInfect*healthy*infected/(healthy+infected+recovered), rateInfect*healthy*infected/(healthy+infected+recovered)-rateRecover*infected, rateRecover*infected]
-        #The equation for Dead will just be (k3*I) where k3 is the death rate, we will have to add (-k3*I) to the infected part of the graph.
-    return solve_ivp(abc, [0, maxtime], [h_0,i_0,r_0], t_eval=np.arange(0, maxtime, 1),method='Radau')
+        """
+        healthy, infected, recovered = y
+        return [
+            -rateInfect * healthy * infected / (healthy + infected + recovered),
+            rateInfect * healthy * infected / (healthy + infected + recovered)
+            - rateRecover * infected,
+            rateRecover * infected,
+        ]
+        # The equation for Dead will just be (k3*I) where k3 is the death rate, we will have to add (-k3*I) to the infected part of the graph.
+
+    return solve_ivp(
+        abc,
+        [0, maxtime],
+        [h_0, i_0, r_0],
+        t_eval=np.arange(0, maxtime, 1),
+        method="Radau",
+    )
+
+
 filename = sys.argv[1]
 
-time = np.loadtxt(filename, skiprows = 8, delimiter = ',', usecols = (0))
-infected = np.loadtxt(filename, skiprows = 8, delimiter = ',', usecols = (2))
-dead = np.loadtxt(filename, skiprows = 8, delimiter = ',', usecols = (4))
-recovered = np.loadtxt(filename, skiprows = 8,  delimiter = ',', usecols = (6))
-solution = kinetics(0.3,0.04, pop-1,1,0) #ONE infected person on day0
+time = np.loadtxt(filename, skiprows=8, delimiter=",", usecols=(0))
+infected = np.loadtxt(filename, skiprows=8, delimiter=",", usecols=(2))
+dead = np.loadtxt(filename, skiprows=8, delimiter=",", usecols=(4))
+recovered = np.loadtxt(filename, skiprows=8, delimiter=",", usecols=(6))
+solution = kinetics(0.3, 0.04, pop - 1, 1, 0)  # ONE infected person on day0
 
-plt.plot(time, dead, label='Real Dead', color = 'k')
-plt.plot(time,infected, label='Real Infections')
-plt.plot(time,recovered, label='Real Recovered', color = 'm')
-plt.xlabel('Time [days]')
-plt.ylabel('Population infected')
+plt.plot(time, dead, label="Real Dead", color="k")
+plt.plot(time, infected, label="Real Infections")
+plt.plot(time, recovered, label="Real Recovered", color="m")
+plt.xlabel("Time [days]")
+plt.ylabel("Population infected")
 
-plt.title('Infection Rate of COVID-19 in Idaho:\nModel VS Data')
+plt.title("Infection Rate of COVID-19 in Idaho")
 plt.grid()
 
-plt.legend(loc='best')
+plt.legend(loc="best")
 plt.show()
 
+plt.plot(solution.t, solution.y[0], label="Model Susceptible")
+plt.plot(solution.t, solution.y[1], label="Model Infected")
+plt.plot(solution.t, solution.y[2], label="Model Recovered")
+plt.legend()
+plt.xlabel("Time [days]")
+plt.ylabel("Number of People")
+plt.title("Infection Rate of COVID-19 in Idaho:\n SIR model")
+plt.savefig("Images/Model_Data.png")
+plt.show()
